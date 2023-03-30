370fb3bdd4f0b12721d756f4fe6d3904bc2f55d8
ptpyip <yippaco123123@gmail.com>
2023-03-30 11:59:04 +0800

    add training pipeline

---
+++ FRCNN/train.py
@@ -0,0 +1,159 @@
+'''
+    Follow the torchvision object detection finetuning tutorial
+    (https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html)
+'''
+import torch
+from torch.utils.data import DataLoader 
+from torch.optim import lr_scheduler 
+
+import torchvision as tv
+from torchvision.models.detection.faster_rcnn import FastRCNNConvFCHead
+from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn_v2 as faster_rcnn
+from torchvision.models.detection.ssd import ssd300_vgg16 as ssd
+
+# detection module from Torchversion/references/detection (download required)
+from ..detection import transforms as T
+from detection import utils           
+from detection.engine import train_one_epoch, evaluate
+
+# self-developed module
+from dataloader import myVOCDetection, getVOCDataLoader
+from utils import parse_args
+
+### 0. Preperation 
+''''
+Download TorchVision repo to use some files from references/detection
+!git clone https://github.com/pytorch/vision.git
+or 
+upload detection
+'''
+
+CLASSES = ['Background', 'person', 'hat']        # Background is default for Pytorch, custom class must start with 1
+
+# handle user input 
+# CLI setup based on GitHub repo
+# (https://github.com/njvisionpower/Safety-Helmet-Wearing-Dataset/blob/master/train_yolo.py)
+
+# data augmentation
+def getTransforms(is_train: bool = False):
+    transforms = []
+    transforms.append(T.PILToTensor())
+    transforms.append(T.ConvertImageDtype(torch.float))
+    
+    # Data augmentation 
+    if is_train:
+        #Nomalization is done implicitly 
+        transforms.append(T.RandomAffine( 
+            degrees = 10, translate = (0.1, 0.1), 
+            scale = (0.9, 1.9) ,shear = 10
+        ))
+        transforms.append(T.RandomHorizontalFlip(p=0.5))               # applied to 
+    return T.Compose(transforms)
+
+### 2. Finetuning from a pretrained model
+def getModel(args, num_classes):
+    model = None
+    if args.model == 'ssd':
+        model = ssd(weights="DEFAULT")
+    if args.model == 'faste_rcnn':
+        model = faster_rcnn(weights="DEFAULT")
+    return model
+
+def train(model, data_loaders, args):
+    train_data_loader, val_data_loader = data_loaders
+    
+    device = torch.device('cpu')
+    if torch.cuda.is_available(): 
+        device = torch.device('cuda') 
+        
+    model.to(device) 
+    optimizer = getOptimizer(model, args)
+    scheduler = getScheduler(optimizer, args)
+    
+    start_epoch = 0
+    if len(args.resume) != 0:                   # add resume functionality
+        args.save_path = f"{args.resume.rsplit('_', maxsplit=1)[0]}.pt"
+        checkpoint = torch.load(args.resume)        # force load back to gpu
+        
+        start_epoch = checkpoint['epoch'] + 1
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_dict'])
+     
+    for epoch in range(start_epoch, args.epochs):    
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, train_data_loader, 
+                        device, epoch, print_freq=args.log_interval)
+        # print("done 1 ep")
+        # break
+        scheduler.step()
+        evaluate(model, val_data_loader, device=device)
+        
+        if epoch%args.save_interval == 0:
+            save_path = f"{args.save_path.rstrip('.pt')}_{epoch+1}.pt"
+            checkpoint = {
+                'epoch'             : epoch,
+                'model_state_dict'  : model.state_dict(),
+                'optimizer_dict'    : optimizer.state_dict(),
+                'scheduler_dict'    : scheduler.state_dict(),
+            }
+            torch.save(checkpoint, save_path)
+            
+    torch.save(model.state_dict(), args.save_path)
+    return
+    
+def testTrain():
+    from torchvision import transforms
+
+    transform = transforms.Compose([
+        # you can add other transformations in this list
+        transforms.ToTensor()
+    ])
+    train_dataset = myVOCDetection('/content/', '2028', 
+                                   image_set='train', classes=CLASSES,
+                                   transforms=getTransforms(is_train=True))
+        
+    data_loader = DataLoader(train_dataset, 
+                                batch_size=2, shuffle=True, 
+                                num_workers=0)
+    model = getModel(num_classes=2)
+    
+    images, targets = next(iter(data_loader))
+    images = [image for image in images]
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    print(targets[0].keys())
+    output = model(images, targets) 
+    model.eval()    
+    
+    return
+
+def getOptimizer(model, args):
+    learning_rate = args.lr
+    momentum = args.momentum
+    weight_decay = args.wd
+    
+    # for possible fintuning optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    return torch.optim.SGD(params, learning_rate, momentum, 
+                           weight_decay=weight_decay)
+    
+def getScheduler(optimizer, args):
+    if args.lr_mode == "step":
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=3, gamma=args.lr_decay)
+    return scheduler
+
+def main():
+    args = parse_args()
+    if args.dataset != 'voc': 
+        print("Only support VOC dataset!")
+        
+    torch.manual_seed(args.seed)
+    data_loaders = getVOCDataLoader(args)
+    model = getModel(args, num_classes=2)     # exclude background
+    
+    train(model, data_loaders, args)
+    
+    return
+
+if __name__ == "__main__":
+    main() 
---
+++ FRCNN/utils.py
@@ -0,0 +1,66 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train the networks with random input shape.')
+    parser.add_argument('--model', type=str, default='faste_rcnn',
+                        #ssd  mobilenet1.0 mobilenet0.25
+                        help="Base network name which serves as feature extraction base.")
+    parser.add_argument('--batch-size', type=int, default=4,
+                        help='Training mini-batch size')
+    parser.add_argument('--dataset', type=str, default='voc',
+                        help='Training dataset. Now support voc.')
+    parser.add_argument('--dataset-dir', type=str, default='../content/VOC2028',
+                        help='Dirctary of training dataset.')
+    parser.add_argument('--num-workers', '-j', dest='num_workers', type=int,
+                        default=4, help='Number of data workers, you can use larger '
+                        'number to accelerate data loading, if you CPU and GPUs are powerful.')
+    parser.add_argument('--gpus', type=str, default='1',
+                        help='Training with GPUs, you can specify 1,3 for example.')
+    parser.add_argument('--epochs', type=int, default=3,
+                        help='Training epochs.')
+    parser.add_argument('--resume', type=str, default='',
+                        help='Resume from previously saved parameters if not None. '
+                        'For example, you can resume from ./yolo3_xxx_0123.pt')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='Learning rate, default is 0.001')
+    parser.add_argument('--lr-mode', type=str, default='step',
+                        help='learning rate scheduler mode. options are step, poly and cosine.')
+    parser.add_argument('--lr-decay', type=float, default=0.1,
+                        help='decay rate of learning rate. default is 0.1.')
+    parser.add_argument('--lr-decay-period', type=int, default=0,
+                        help='interval for periodic learning rate decays. default is 0 to disable.')
+    parser.add_argument('--lr-decay-epoch', type=str, default='60, 80',
+                        help='epochs at which learning rate decays. default is 60,80.')
+    parser.add_argument('--warmup-lr', type=float, default=0.0,
+                        help='starting warmup learning rate. default is 0.0.')
+    parser.add_argument('--warmup-epochs', type=int, default=0,
+                        help='number of warmup epochs.')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='SGD momentum, default is 0.9')
+    parser.add_argument('--wd', type=float, default=0.0005,
+                        help='Weight decay, default is 5e-4')
+    parser.add_argument('--log-interval', type=int, default=100,
+                        help='Logging mini-batch interval. Default is 100.')
+    parser.add_argument('--save-prefix', type=str, default='',
+                        help='Saving parameter prefix')
+    parser.add_argument('--save-interval', type=int, default=2,
+                        help='Saving parameters epoch interval, best model will always be saved.')
+    parser.add_argument('--save-path', type=str, default='/content/ourModel/FRCNN.pt',
+                        help='Saving parameter path')
+    parser.add_argument('--val-interval', type=int, default=1,
+                        help='Epoch interval for validation, increase the number will reduce the '
+                             'training time if validation is slow.')
+    parser.add_argument('--seed', type=int, default=233,
+                        help='Random seed to be fixed.')
+    parser.add_argument('--no-random-shape', action='store_true',
+                        help='Use fixed size(data-shape) throughout the training, which will be faster '
+                        'and require less memory. However, final model will be slightly worse.')
+    parser.add_argument('--no-wd', action='store_true',
+                        help='whether to remove weight decay on bias, and beta/gamma for batchnorm layers.')
+    parser.add_argument('--mixup', action='store_true',
+                        help='whether to enable mixup.')
+    parser.add_argument('--no-mixup-epochs', type=int, default=20,
+                        help='Disable mixup training if enabled in the last N epochs.')
+    parser.add_argument('--label-smooth', action='store_true', help='Use label smoothing.')
+    args = parser.parse_args()
+    return args
